94075d80cab3944e6c8e628471667eb9a9b56dc8
Ronaldo Fidelis <Ronaldofidelis.ti@gmail.com>
2025-08-06 16:30:56 -0300

    feat:ajustes no main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/authProvider.jsx'
 import './style/index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App/>
-    </AuthProvider>
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
 )
