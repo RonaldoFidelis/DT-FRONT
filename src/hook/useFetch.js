afee9756d92dd669bb5cce9821baea9e2d05741a
Ronaldo Fidelis <Ronaldofidelis.ti@gmail.com>
2025-08-06 16:29:39 -0300

    hook fetch

---
+++ src/hook/useFetch.js
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
+import { loginApi, recoverPasswordApi, registeApi } from '../service/useService';
+
+export const useLogin = () => {
+  return useMutation({
+    mutationFn: loginApi
+  });
+};
+
+export const useRegister = () => {
+  return useMutation({
+    mutationFn: registeApi
+  })
+}
+
+export const useRecoverPassword = () => {
+  return useMutation({
+    mutationFn: recoverPasswordApi,
+    retry: 1,
+    retryDelay: () => 5000,
+  })
+}
