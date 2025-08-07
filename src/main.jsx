15a3c64450708b02637a571b42124b4d3eb3e3bb
Ronaldo Fidelis <Ronaldofidelis.ti@gmail.com>
2025-08-07 10:43:21 -0300

    fix: Removendo context desnecessario

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { AuthProvider } from './auth/authProvider.jsx'
-import './style/index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import App from './App.jsx'
+import './style/index.css'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>
 )
